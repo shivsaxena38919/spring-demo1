0d47c35365921adae3999c36e494f602f857aca6
Shivam Saxena <shivsaxena91@gmail.com>
2021-06-28 15:23:47 +0530

    new feature

--- src/main/java/com/example/demo/FirstClass.java
+++ src/main/java/com/example/demo/FirstClass.java
@@ -1,19 +1,32 @@
 package com.example.demo;
 
+import javax.swing.text.View;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FirstClass {
-	
+	ModelAndView mav=new ModelAndView();
 	
 	@RequestMapping
+	public ModelAndView hi() {
+		
+		mav.setViewName("index");
+		
+		return mav;
+		
+	}
+	
+	@RequestMapping("/hi")
 	@ResponseBody
-	public String First() {
+	public String sec() {
 		
 		return "HI first program";
 		
 	}
+
 
 }
